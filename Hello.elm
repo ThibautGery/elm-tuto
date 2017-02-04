e23aee40bf979b8e0c9d89b723591c6ebdaaaa05
Thibaut Gery <thibaut.gery@gmail.com>
2017-02-04 14:32:15 +1100

    try a few commands

--- Hello.elm
+++ Hello.elm
@@ -5,3 +5,21 @@ import Html exposing (text)
 
 main =
     text "Hello"
+
+
+add : Int -> Int -> Int
+add x y =
+  x + y
+
+
+switch : ( qqq, ccc ) -> ( ccc, qqq )
+switch ( x, y ) =
+  ( y, x )
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    }
+
+

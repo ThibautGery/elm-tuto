20b9499dd237db96dd3d9b681b7fbf09cffe6b9f
Thibaut Gery <thibaut.gery@gmail.com>
2017-02-05 20:54:23 +1100

    setup navigation

--- src/Players/Messages.elm
+++ src/Players/Messages.elm
@@ -1,8 +1,10 @@
 module Players.Messages exposing (..)
 
 import Http exposing (Error)
-import Players.Models exposing (Player)
+import Players.Models exposing (Player, PlayerId)
 
 
 type Msg
     = OnFetchAll (Result Http.Error (List Player))
+      | ShowPlayers
+      | ShowPlayer PlayerId
